23a1fc2055096c99f0c034acc46c712e5733b8ff
Nisheshg5 <Nisheshg5@gmail.com>
2020-12-11 02:13:01 +0530

    Added functionality for exit and logout button

---
+++ src/ran/front/HomePageSidePaneController.java
@@ -0,0 +1,32 @@
+package ran.front;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class HomePageSidePaneController {
+
+	@FXML
+	VBox vBox;
+
+	@FXML
+	void logout(ActionEvent e) {
+		AnchorPane root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			vBox.getScene().setRoot(root);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	@FXML
+	void exit(ActionEvent e) {
+		System.exit(0);
+	}
+
+}
