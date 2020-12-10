6e87393b0b9f207f3ea7200de501a4ee3f7d3ce4
Nisheshg5 <Nisheshg5@gmail.com>
2020-12-10 17:13:37 +0530

    Added Hamburger For sidebar

--- src/ran/front/HospitalMS.java
+++ src/ran/front/HospitalMS.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 public class HospitalMS extends Application {
 	public static Stage stage = null;
-	public static Boolean isWelcomeLoaded = false;
+	public static Boolean isWelcomeLoaded = true;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -16,6 +16,7 @@ public class HospitalMS extends Application {
 		Parent root;
 		try {
 			root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add("ran/front/Main.css");
 			primaryStage.setFullScreen(true);
