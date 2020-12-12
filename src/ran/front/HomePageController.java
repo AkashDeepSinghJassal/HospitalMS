f295c5305c4c64584284f78599b6bc9b3dbcb9d2
Nisheshg5 <Nisheshg5@gmail.com>
2020-12-12 17:31:59 +0530

    Added loader to HomePage

--- src/ran/front/HomePageController.java
+++ src/ran/front/HomePageController.java
@@ -4,20 +4,25 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
 
 import javafx.animation.FadeTransition;
 import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
 public class HomePageController implements Initializable {
@@ -27,6 +32,14 @@ public class HomePageController implements Initializable {
 	private JFXHamburger hamburger;
 	@FXML
 	private JFXDrawer drawer;
+	@FXML
+	private Circle c1;
+	@FXML
+	private Circle c2;
+	@FXML
+	private Circle c3;
+
+	ObservableList<JFXButton> sidebuttons;
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
@@ -37,6 +50,31 @@ public class HomePageController implements Initializable {
 		try {
 			VBox sidePane = FXMLLoader.load(getClass().getResource("HomePageSidePane.fxml"));
 			drawer.setSidePane(sidePane);
+
+			for (Node node : sidePane.getChildren()) {
+				if (node.getAccessibleText() != null) {
+					switch (node.getAccessibleText()) {
+						case "logout":
+							node.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+								RotateTransition r1 = setRotate(c1, true, 360, 1500);
+								r1.setOnFinished((e2) -> {
+									logout();
+									System.out.println(anchorPane.getScene());
+								});
+								setRotate(c2, true, -360, 2500);
+								setRotate(c3, true, 360, 3000);
+							});
+							break;
+						case "exit":
+							node.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+								exit();
+							});
+							break;
+						default:
+							break;
+					}
+				}
+			}
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -62,16 +100,16 @@ public class HomePageController implements Initializable {
 		HospitalMS.isWelcomeLoaded = true;
 
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
-			anchorPane.getChildren().setAll(root);
+			Parent newRoot = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
+			anchorPane.getChildren().setAll(newRoot);
 
-			FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), root);
+			FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), newRoot);
 			fadeIn.setFromValue(0);
 			fadeIn.setToValue(1);
 			fadeIn.setCycleCount(1);
 			fadeIn.interpolatorProperty().set(Interpolator.EASE_OUT);
 
-			FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), root);
+			FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), newRoot);
 			fadeOut.setFromValue(1);
 			fadeOut.setToValue(0);
 			fadeOut.setCycleCount(1);
@@ -86,8 +124,8 @@ public class HomePageController implements Initializable {
 
 			fadeOut.setOnFinished((e) -> {
 				try {
-					AnchorPane newRoot = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-					anchorPane.getChildren().setAll(newRoot.getChildren());
+					AnchorPane oldRoot = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+					anchorPane.getChildren().setAll(oldRoot.getChildren());
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
@@ -96,5 +134,29 @@ public class HomePageController implements Initializable {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	void logout() {
+		HospitalMS.isWelcomeLoaded = false;
+		AnchorPane root;
+		try {
+		root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+		anchorPane.getScene().setRoot(root);
+		} catch (IOException e1) {
+		e1.printStackTrace();
+		}
+	}
+
+	void exit() {
+		System.exit(0);
+	}
+
+	RotateTransition setRotate(Circle circle, boolean reverse, int angle, int duration) {
+		RotateTransition rotateTransition = new RotateTransition(Duration.millis(duration), circle);
+		rotateTransition.setInterpolator(Interpolator.LINEAR);
+		rotateTransition.setByAngle(angle);
+		rotateTransition.setCycleCount(1);
+		rotateTransition.play();
+		return rotateTransition;
 	}
 }
