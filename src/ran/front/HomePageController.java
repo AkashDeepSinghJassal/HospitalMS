e0b15e355eabc48677a7ea640fd8175fc212a5d4
Nisheshg5 <Nisheshg5@gmail.com>
2020-12-12 18:29:14 +0530

    Seperated content to methods

--- src/ran/front/HomePageController.java
+++ src/ran/front/HomePageController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
@@ -12,7 +11,6 @@ import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
 import javafx.animation.FadeTransition;
 import javafx.animation.Interpolator;
 import javafx.animation.RotateTransition;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -39,14 +37,57 @@ public class HomePageController implements Initializable {
 	@FXML
 	private Circle c3;
 
-	ObservableList<JFXButton> sidebuttons;
-
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		if (!HospitalMS.isWelcomeLoaded) {
 			loadWelcomePage();
 		}
 
+		loadSidePane();
+		setHamburger();
+
+		hamburger.fireEvent(new MouseEvent(MouseEvent.MOUSE_CLICKED, 1, 2, 3, 4, MouseButton.PRIMARY, 5, true, true,
+				true, true, true, true, true, true, true, true, null));
+	}
+
+	void loadWelcomePage() {
+		HospitalMS.isWelcomeLoaded = true;
+
+		try {
+			Parent newRoot = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
+			anchorPane.getChildren().setAll(newRoot);
+
+			FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), newRoot);
+			fadeIn.setFromValue(0);
+			fadeIn.setToValue(1);
+			fadeIn.setCycleCount(1);
+			fadeIn.interpolatorProperty().set(Interpolator.EASE_OUT);
+
+			FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), newRoot);
+			fadeOut.setFromValue(1);
+			fadeOut.setToValue(0);
+			fadeOut.setCycleCount(1);
+			fadeOut.setDelay(Duration.seconds(1));
+			fadeIn.interpolatorProperty().set(Interpolator.EASE_IN);
+			fadeIn.play();
+
+			fadeIn.setOnFinished((e) -> {
+				fadeOut.play();
+			});
+			fadeOut.setOnFinished((e) -> {
+				try {
+					AnchorPane oldRoot = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+					anchorPane.getChildren().setAll(oldRoot.getChildren());
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void loadSidePane() {
 		try {
 			VBox sidePane = FXMLLoader.load(getClass().getResource("HomePageSidePane.fxml"));
 			drawer.setSidePane(sidePane);
@@ -74,10 +115,12 @@ public class HomePageController implements Initializable {
 					}
 				}
 			}
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+	}
 
+	public void setHamburger() {
 		HamburgerBasicCloseTransition transition = new HamburgerBasicCloseTransition(hamburger);
 		transition.setRate(-1);
 		hamburger.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
@@ -90,59 +133,16 @@ public class HomePageController implements Initializable {
 				drawer.open();
 			}
 		});
-
-		hamburger.fireEvent(new MouseEvent(MouseEvent.MOUSE_CLICKED, 1, 2, 3, 4, MouseButton.PRIMARY, 5, true, true,
-				true, true, true, true, true, true, true, true, null));
-	}
-
-	void loadWelcomePage() {
-		HospitalMS.isWelcomeLoaded = true;
-
-		try {
-			Parent newRoot = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
-			anchorPane.getChildren().setAll(newRoot);
-
-			FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), newRoot);
-			fadeIn.setFromValue(0);
-			fadeIn.setToValue(1);
-			fadeIn.setCycleCount(1);
-			fadeIn.interpolatorProperty().set(Interpolator.EASE_OUT);
-
-			FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), newRoot);
-			fadeOut.setFromValue(1);
-			fadeOut.setToValue(0);
-			fadeOut.setCycleCount(1);
-			fadeOut.setDelay(Duration.seconds(1));
-			fadeIn.interpolatorProperty().set(Interpolator.EASE_IN);
-
-			fadeIn.play();
-
-			fadeIn.setOnFinished((e) -> {
-				fadeOut.play();
-			});
-
-			fadeOut.setOnFinished((e) -> {
-				try {
-					AnchorPane oldRoot = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-					anchorPane.getChildren().setAll(oldRoot.getChildren());
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-
-			});
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 	void logout() {
 		HospitalMS.isWelcomeLoaded = false;
 		AnchorPane root;
 		try {
-		root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-		anchorPane.getScene().setRoot(root);
-		} catch (IOException e1) {
-		e1.printStackTrace();
+			root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			anchorPane.getScene().setRoot(root);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
