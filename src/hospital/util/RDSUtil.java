5631b388006f3ec3041b2c28987bd64a705e5241
AkashDeepSinghJassal <akashdeepjassal@gmail.com>
2021-02-02 01:52:31 +0530

    changed rds

--- src/hospital/util/RDSUtil.java
+++ src/hospital/util/RDSUtil.java
@@ -2,20 +2,24 @@ package hospital.util;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 
 public class RDSUtil {
 	static String getRdsCredential() {
 		RDSCredential rdsCredential;
 		try {
+//			FileOutputStream fos = new FileOutputStream("rdscredential.ser");
+//			ObjectOutputStream oos = new ObjectOutputStream(fos);
+//			oos.writeObject(rdsCredential);
 			FileInputStream fis = new FileInputStream("rdscredential.ser");
 			ObjectInputStream ois = new ObjectInputStream(fis);
 			rdsCredential = (RDSCredential) ois.readObject();
 			fis.close();
 			ois.close();
-			rdsCredential = new RDSCredential("quiz-mysql-db.mysql.database.azure.com", "hospital", "adminsql@quiz-mysql-db", "Quiz@123");
 			return rdsCredential.getCredentials();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
